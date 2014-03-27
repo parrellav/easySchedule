d1f565a66ec9e61c004e75410aaddb2348cb1f62
Parrella, Vincent <Vincent_Parrella@cable.comcast.com>
2014-03-27 11:24:57 -0400

    changing dates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,21 @@
 'use strict';
 
 angular.module('easyScheduleApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'ui.calendar',
-  'scheduleControllers',
-  'mgcrea.ngStrap'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'ui.calendar',
+        'scheduleControllers',
+        'mgcrea.ngStrap'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
