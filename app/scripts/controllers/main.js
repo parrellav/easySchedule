4b2e32375205a2597154e2c3e65dfb74926b9b00
Parrella, Vincent <Vincent_Parrella@cable.comcast.com>
2014-03-28 09:06:09 -0400

    using real data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,32 +2,52 @@
 
 var scheduleControllers = angular.module('scheduleControllers', []);
 
-scheduleControllers.controller('MainCtrl', function ($scope) {
+scheduleControllers.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    var url = 'http://localhost:3000/events';
 
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
+    $scope.events = [];
+    $scope.eventSources = [];
 
     /* event source that contains custom events on the scope */
-    $scope.events = [
-        {title: 'All Day Event',start: new Date(y, m, 1)},
-        {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-        {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-        {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
+//    $scope.events = [
+//        {title: 'All Day Event',start: new Date(y, m, 1)},
+//        {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+//        {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+//        {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+//        {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+//    ];
+//    console.log($scope.events);
+
+
+    $http.get(url).
+        success(function(data, status, headers, config) {
+            for(var i = 0; i < data.length; i++) {
+                $scope.events.push(data[i]);
+            }
+            $scope.eventSources.push($scope.events);
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
 
     /* add custom event*/
     $scope.addEvent = function() {
-        $scope.events.push({
+        var newEvent =
+        {
             title: 'Untitled Event',
             start: new Date(y, m, d, 8, 0, 0),
             end: new Date(y, m, d, 12, 0, 0),
             allDay: false,
             className: ['openSesame']
-        });
+        }
+        $http.post(url, newEvent).success(addToCalendar);
+
     };
 
     /* remove event */
@@ -53,25 +73,13 @@ scheduleControllers.controller('MainCtrl', function ($scope) {
                 left: 'title',
                 center: '',
                 right: 'today prev,next'
-            },
-            eventClick: $scope.alertOnEventClick,
+            }
         }
     };
 
-    $scope.eventSources = [$scope.events];
-  });
+    var addToCalendar = function(data, status, headers, config) {
+        $scope.events.push(data);
+    }
 
-scheduleControllers.controller('AddEventCtrl', function($scope) {
-    $scope.hi = 'add';
+  }]);
 
-    $scope.today = function() {
-        $scope.dt = new Date();
-    };
-    $scope.today();
-
-    $scope.toggleMin = function() {
-        $scope.minDate = ( $scope.minDate ) ? null : new Date();
-    };
-    $scope.toggleMin();
-
-});
